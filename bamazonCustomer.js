833c1c90cdc5ac37e1702239986c8c5f61c8777a
Denzel <denzelhartshorn@gmail.com>
2019-08-12 21:00:16 -0700

    set up function for item and started quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,37 +1,70 @@
-//is bamazonDB the right thing to put in here?  or is it "mysql" or is it the ../ source
-let bamazonDB = require('bamazonDB');
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+require("console.table");
 
-let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'bamazonDB'
+
+var connection = mysql.createConnection({
+    host:"localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon"
 });
 
-connection.connect(function (err) {
+connection.connect(function(err) {
     if (err) {
-        return console.error('error: ' + err.message);
+        console.error("error connecting: " + err.stack);
     }
-
-    //not sure if this is correct, i copied it from node's website
-    let createProducts = `create table if not exists todos(
-        item_id INT NOT NULL AUTO_INCREMENT,
-        product_name VARCHAR(45) NULL,
-        department_name VARCHAR(45) NULL,
-        price INTEGER NOT NULL,
-        stock_quantity INTEGER NOT NULL,
-        PRIMARY KEY (id)
-                        )`;
-
-    connection.query(createProducts, function (err, results, fields) {
-        if (err) {
-            console.log(err.message);
-        }
-    });
-
-    connection.end(function (err) {
-        if (err) {
-            return console.log(err.message);
-        }
-    });
+    loadProducts();
 });
+
+function loadProducts() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        promptCustomerForItem(res);
+    });
+}
+
+function promptCustomerForItem(inventory) {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "choice",
+                message: "What is the ID of the item you would like to purchase [Q to quit]",
+                validate: function(val) {
+                    return !isNaN(val) || val.toLowerCase() === "q";
+                }
+            }
+        ])
+        .then(function(val) {
+            checkIfShouldExit(val.choice);
+            var choiceId = parseInt(val.choice);
+            var product = checkInventory(choiceId, inventory);
+
+            if (product) {
+                promptCustomerForQuantity(product);
+            }
+            else {
+                //why is there "\n"???
+                console.log("\nThat item is not in the inventory");
+                loadProducts();
+            }
+        });
+}
+
+function promptCustomerForQuantity(product) {
+    inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "quantity",
+            message: "how many do you want? [Q to Quit]",
+            validate: function(val) {
+                return val > 0 || val.toLowerCase() === "q";
+            }
+        }
+    ])
+    .then(function(val))
+}
